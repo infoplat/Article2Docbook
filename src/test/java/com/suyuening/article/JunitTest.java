076d8acbf06db6cbf6453e1f4507c1906ac4c389
infoplat <infoplat@163.com>
2016-06-12 16:14:29 +0800

    修改文件换行符为linux换行符

--- src/test/java/com/suyuening/article/JunitTest.java
+++ src/test/java/com/suyuening/article/JunitTest.java
@@ -1,13 +1,25 @@
 package com.suyuening.article;
 
-import junit.framework.Assert;
+import java.util.List;
 
 import org.junit.Test;
+
+import com.google.common.collect.Lists;
+import com.suyuening.article.bean.DocBookSection;
+
+import junit.framework.Assert;
 
 public class JunitTest {
 
     @Test
     public void testJunitTset() {
         Assert.assertEquals(true, true);
+
+        List<String> paraList = Lists.asList("一", new String[] {"二", "三"});
+        DocBookSection dbs = new DocBookSection("测试文章");
+        dbs.addPara("四");
+        dbs.addPara("五");
+        dbs.addMutiParas(paraList);
+        dbs.printInfo();
     }
 }
